90b99710c62d189f8ef440e7a31c0083f693979a
Chris Salley <csalley1995@gmail.com>
2026-09-12 09:17:02 -0400

    Added a header element using js.

--- dom.js
+++ dom.js
@@ -13,3 +13,8 @@ paragraph.style.color = "red";
 
 container.appendChild(paragraph);
 
+const heading3 = document.createElement("h3");
+heading3.textContent = "I'm a blue h3!";
+heading3.style.color = "blue";
+
+container.appendChild(heading3);
